0bdc03aa365e267d2c465d2900e0d8e88f5daf58
Kawoonchik <sydor.anatolii@gmail.com>
2025-12-29 13:01:10 +0200

    Initial commit

---
+++ yoy nay bude 2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введіть текст англіською(r,k,t):");
+        string input = Console.ReadLine();
+        Char[] text = input.ToCharArray();
+
+        int CountR = 0, CountK = 0, CountT = 0;
+
+        foreach (char c in text)
+        {
+            switch (char.ToLower(c))
+            {
+                case 'r':
+                    CountR++;
+                    break;
+                case 'k':
+                    CountK++;
+                    break;
+                case 't':
+                    CountT++;
+                    break;
+            }
+        }
+
+        Console.WriteLine("Результае:");
+        Console.WriteLine($"Кількість r: {CountR}");    
+        Console.WriteLine($"Кількість k: {CountK}");
+        Console.WriteLine($"Кількість t: {CountT}");
+    }
+}   
---
+++ yoy nay bude/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введіть число від 1 до 365 (1 < k < 365):");
+        string input = Console.ReadLine();
+
+        if (int.TryParse(input, out int k) && k >= 1 && k <= 365)
+        {
+            int dayIndex = (k - 1) % 7;
+            string dayName = "";
+            
+            switch (dayIndex)
+            {
+                case 0:
+                    dayName = "Понеділок";
+                    break;
+                case 1:
+                    dayName = "Вівторок";
+                    break;
+                case 2:
+                    dayName = "Середа";
+                    break;
+                case 3:
+                    dayName = "Четвер";
+                    break;
+                case 4:
+                    dayName = "П'ятниця";
+                    break;
+                case 5:
+                    dayName = "Субота";
+                    break;
+                case 6:
+                    dayName = "Неділя";
+                    break;
+            }
+            Console.WriteLine($"День тижня для числа {k}: {dayName}");
+        }
+
+        else
+        {
+            Console.WriteLine("Помилка, число за межами. (1 < k < 365)");
+        }
+    }
+}
